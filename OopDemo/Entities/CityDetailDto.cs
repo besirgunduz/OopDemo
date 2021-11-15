d0266de0a5014981d65a8c878c9bff7ab976c562
Besir Gunduz <besirgunduz1993@gmail.com>
2021-11-15 19:46:18 +0300

    added CityDetailDto

---
+++ OopDemo/Entities/CityDetailDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OopDemo.Entities
+{
+    public class CityDetailDto : BaseEntity
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+        public string UserName { get; set; }
+        public UserRole UserRole { get; set; }
+    }
+}
