fb74c21e0206c4b189d3a7ec1ed7f588ea38ff4f
Besir Gunduz <besirgunduz1993@gmail.com>
2021-11-15 19:47:10 +0300

    added User

---
+++ OopDemo/Entities/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OopDemo.Entities
+{
+    public class User : BaseEntity, IEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public UserRole UserRole { get; set; }
+        public virtual ICollection<City> Cities { get; set; }
+
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Saçmalama dostum, yaş sıfırdan küçük olamaz!");
+                }
+
+                age = value;
+            }
+        }
+    }
+}
