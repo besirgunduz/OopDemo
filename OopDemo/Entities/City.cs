8cda781a463b00d2b7b98960bce86ea1a1dd7a22
Besir Gunduz <besirgunduz1993@gmail.com>
2021-11-15 19:45:57 +0300

    added City

---
+++ OopDemo/Entities/City.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace OopDemo.Entities
+{
+    public class City : BaseEntity, IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<Photo> Photos { get; set; }
+        public virtual User User { get; set; }
+
+    }
+}
