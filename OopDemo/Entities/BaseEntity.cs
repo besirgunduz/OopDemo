e1440f1d0cfd7d9468741ee48963d2c2c97dbdeb
Besir Gunduz <besirgunduz1993@gmail.com>
2021-11-15 19:45:06 +0300

    added BaseEntity

---
+++ OopDemo/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OopDemo.Entities
+{
+    public class BaseEntity
+    {
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
