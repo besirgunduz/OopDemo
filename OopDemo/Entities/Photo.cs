25cb3426ade80f3c402a6c08f125720f5316bb3e
Besir Gunduz <besirgunduz1993@gmail.com>
2021-11-15 19:46:53 +0300

    added Photo

---
+++ OopDemo/Entities/Photo.cs
@@ -0,0 +1,12 @@
+﻿namespace OopDemo.Entities
+{
+    public class Photo : BaseEntity, IEntity
+    {
+        public int Id { get; set; }
+        public int CityId { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public virtual City City { get; set; }
+
+    }
+}
